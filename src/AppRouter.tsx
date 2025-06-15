01187080541538bf033783943ce663a0c5575b4d
galeed gutierrez <galeedeveloper@gmail.com>
2025-06-15 05:46:22 -0300

    feat: add AppRouter component for application routing setup

---
+++ src/AppRouter.tsx
@@ -0,0 +1,48 @@
+import { AppRoutes } from '@models/routes/appRouters.model';
+import { Home } from '@views/Home';
+import { RoutesWithNotFound } from '@views/NotFoundPage';
+import type { JSX } from 'react';
+import { BrowserRouter, Route } from 'react-router';
+
+import { Layout } from './views/Layout';
+
+/**
+ * Main application router.
+ * This component sets up the routing for the application using React Router.
+ * It defines the routes and their corresponding components.
+ */
+function AppRouter(): JSX.Element {
+	return (
+		<BrowserRouter>
+			<RoutesWithNotFound>
+				<Route
+					element={<Layout />}
+					path={AppRoutes.home}
+				>
+					<Route
+						element={<Home />}
+						path={AppRoutes.home}
+					/>
+					{/* <Route
+					element={<Favorites />}
+					path={AppRoutes.favorites}
+				/> */}
+					{/* <Route
+					element={<ProductsDetails />}
+					path={AppRoutes.products.productsDetails}
+				/> */}
+					{/* <Route
+						element={<AddNewProduct />}
+						path={AppRoutes.products.addNewProduct}
+					/> */}
+					{/* <Route
+					element={<UpdateProduct />}
+					path={AppRoutes.products.updateProduct}
+				/> */}
+				</Route>
+			</RoutesWithNotFound>
+		</BrowserRouter>
+	);
+}
+
+export default AppRouter;
