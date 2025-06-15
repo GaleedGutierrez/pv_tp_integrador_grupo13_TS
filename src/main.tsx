4b69a5fba3b9096c888cbeb2df7a445e41695108
galeed gutierrez <galeedeveloper@gmail.com>
2025-06-15 05:41:34 -0300

    feat: update main entry point to use AppRouter and ErrorBoundary, and load custom fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,24 @@
-import './index.css';
+import '@styles/global.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App.tsx';
+import AppRouter from './AppRouter.tsx';
+import { ErrorBoundary } from './components/ErrorBoundary.tsx';
+import { loadFonts } from './utils/loadFonts.ts';
+
+loadFonts(['1em Satoshi', '700 1em IntegralCF']);
 
 const ROOT = document.querySelector('#root');
 
 if (ROOT) {
 	createRoot(ROOT).render(
 		<StrictMode>
-			<App />
+			{/* <Provider store={store}> */}
+			<ErrorBoundary>
+				<AppRouter />
+			</ErrorBoundary>
+			{/* </Provider> */}
 		</StrictMode>,
 	);
 }
