8005e2ea04ac27ddbf3b3c64c1fc97e8e4460681
galeed gutierrez <galeedeveloper@gmail.com>
2025-06-16 00:57:14 -0300

    feat: remove Product-related components and models to streamline codebase

--- src/models/routes/appRouters.model.ts
+++
@@ -1,9 +0,0 @@
-export const AppRoutes = {
-	home: '/',
-	products: {
-		productsDetails: '/products/:productId',
-		addNewProduct: '/products/nuevo',
-		updateProduct: '/products/:productId/editar',
-	},
-	favorites: '/favoritos',
-} as const;
