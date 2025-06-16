d7891ea3f20a264af4c2a73d3e47d2b5efc3959e
galeed gutierrez <galeedeveloper@gmail.com>
2025-06-16 03:40:39 -0300

    feat: wrap AppRouter with Provider for Redux store integration

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import '@styles/global.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import AppRouter from './AppRouter.tsx';
 import { ErrorBoundary } from './components/ErrorBoundary.tsx';
+import { store } from './store/store.ts';
 import { loadFonts } from './utils/loadFonts.ts';
 
 loadFonts(['1em Satoshi', '700 1em IntegralCF']);
@@ -14,11 +16,11 @@ const ROOT = document.querySelector('#root');
 if (ROOT) {
 	createRoot(ROOT).render(
 		<StrictMode>
-			{/* <Provider store={store}> */}
-			<ErrorBoundary>
-				<AppRouter />
-			</ErrorBoundary>
-			{/* </Provider> */}
+			<Provider store={store}>
+				<ErrorBoundary>
+					<AppRouter />
+				</ErrorBoundary>
+			</Provider>
 		</StrictMode>,
 	);
 }
