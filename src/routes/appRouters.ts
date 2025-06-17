8f95efee4e04e1c5d23eb5fc42127aea5862aed7
galeed gutierrez <galeedeveloper@gmail.com>
2025-06-17 00:40:29 -0300

    refactor: restructure products route to include buildUrl for improved routing functionality

--- src/routes/appRouters.ts
+++ src/routes/appRouters.ts
@@ -1,9 +1,12 @@
 import { FavoriteRoutes } from '@modules/favorites/infrastructure/FavoritesRoutes';
 import { HomeRoutes } from '@modules/home/infrastructure/HomeRoutes';
-import { ProductRoutes } from '@modules/products/infrastructure/ProductRoutes';
+import {
+	buildUrl as ProductRoutesBuildUrl,
+	ProductRoutes,
+} from '@modules/products/infrastructure/ProductRoutes';
 
 export const appRoutes = {
 	home: HomeRoutes,
-	products: ProductRoutes,
+	products: { routes: ProductRoutes, buildUrl: ProductRoutesBuildUrl },
 	favorites: FavoriteRoutes,
 } as const;
