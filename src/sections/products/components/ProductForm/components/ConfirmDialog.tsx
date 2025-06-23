4d5d15a45805717a1e41761b12938385f146078b
galeed gutierrez <galeedeveloper@gmail.com>
2025-06-23 17:15:10 -0300

    feat: update ConfirmDialog to dynamically set dialog title based on mode

--- src/sections/products/components/ProductForm/components/ConfirmDialog.tsx
+++ src/sections/products/components/ProductForm/components/ConfirmDialog.tsx
@@ -19,6 +19,7 @@ interface Properties {
 	onDialogCancel: () => void;
 	onConfirm: () => void;
 	pendingData?: ProductFormData;
+	mode: 'create' | 'update';
 }
 
 /** Dialog to confirm product creation with pending data. */
@@ -28,54 +29,63 @@ export const ConfirmDialog = ({
 	onDialogCancel: handleDialogCancel,
 	onConfirm: handleConfirm,
 	pendingData,
-}: Properties): JSX.Element => (
-	<AlertDialog
-		open={isDialogOpen}
-		onOpenChange={handleDialogOpenChange}
-	>
-		<AlertDialogContent>
-			<AlertDialogHeader>
-				<AlertDialogTitle>Creación de nuevo producto</AlertDialogTitle>
-				<AlertDialogDescription className="text-base">
-					Revisa los datos antes de confirmar
-				</AlertDialogDescription>
-			</AlertDialogHeader>
-			{pendingData && (
-				<div className="mt-4 space-y-2 text-left">
-					<div className="rounded-sm rounded-xl bg-gray-100 p-7">
-						<img
-							alt={pendingData.title}
-							className="max-m-36 max-h-36 justify-self-center mix-blend-multiply"
-							src={pendingData.image}
-						/>
+	mode = 'create',
+}: Properties): JSX.Element => {
+	const DIALOG_TITLE =
+		mode === 'create'
+			? 'Creación de nuevo producto'
+			: 'Actualización de producto';
+
+	return (
+		<AlertDialog
+			open={isDialogOpen}
+			onOpenChange={handleDialogOpenChange}
+		>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle>{DIALOG_TITLE}</AlertDialogTitle>
+					<AlertDialogDescription className="text-base">
+						Revisa los datos antes de confirmar
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+				{pendingData && (
+					<div className="mt-4 space-y-2 text-left">
+						<div className="rounded-sm rounded-xl bg-gray-100 p-7">
+							<img
+								alt={pendingData.title}
+								className="max-m-36 max-h-36 justify-self-center mix-blend-multiply"
+								src={pendingData.image}
+							/>
+						</div>
+						<p>
+							<strong>Título:</strong> {pendingData.title}
+						</p>
+						<p>
+							<strong>Precio:</strong> ${pendingData.price}
+						</p>
+						<p className="capitalize">
+							<strong>Categoría:</strong> {pendingData.category}
+						</p>
+						<p>
+							<strong>Descripción:</strong>{' '}
+							{pendingData.description}
+						</p>
 					</div>
-					<p>
-						<strong>Título:</strong> {pendingData.title}
-					</p>
-					<p>
-						<strong>Precio:</strong> ${pendingData.price}
-					</p>
-					<p className="capitalize">
-						<strong>Categoría:</strong> {pendingData.category}
-					</p>
-					<p>
-						<strong>Descripción:</strong> {pendingData.description}
-					</p>
-				</div>
-			)}
-			<AlertDialogFooter>
-				<AlertDialogCancel asChild>
-					<Button
-						variant="secondary"
-						onClick={handleDialogCancel}
-					>
-						Cancelar
-					</Button>
-				</AlertDialogCancel>
-				<AlertDialogAction onClick={handleConfirm}>
-					Confirmar
-				</AlertDialogAction>
-			</AlertDialogFooter>
-		</AlertDialogContent>
-	</AlertDialog>
-);
+				)}
+				<AlertDialogFooter>
+					<AlertDialogCancel asChild>
+						<Button
+							variant="secondary"
+							onClick={handleDialogCancel}
+						>
+							Cancelar
+						</Button>
+					</AlertDialogCancel>
+					<AlertDialogAction onClick={handleConfirm}>
+						Confirmar
+					</AlertDialogAction>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
+	);
+};
