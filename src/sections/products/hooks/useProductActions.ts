7a3f6c4964daa65c9a069bdfaab9e95d64143653
galeed gutierrez <galeedeveloper@gmail.com>
2025-06-23 16:22:58 -0300

    refactor: clean up useProductActions by removing commented code
    restoring addNewProduct functionality

--- src/sections/products/hooks/useProductActions.ts
+++ src/sections/products/hooks/useProductActions.ts
@@ -1,38 +1,24 @@
 import { useAppDispatch } from '@hooks/useAppDispatch';
 
-// import { useProductDependencies } from '../hooks/useProductDependencies';
-import {
-	// addAllProducts as addAllOfProducts,
-	deleteProductById,
-} from '../slice/productSlice';
+import type { Product } from '@/modules/products/domain/Product';
+
+import { addProduct, deleteProductById } from '../slice/productSlice';
 
 interface ReturnProductActions {
 	deleteProduct: (id: number) => void;
-	// addAllProducts: () => Promise<void>;
+	addNewProduct: (product: Omit<Product, 'rating'>) => void;
 }
-
-// const { getAllProducts } = useProductDependencies();
 
 export const useProductActions = (): ReturnProductActions => {
 	const dispatch = useAppDispatch();
+
+	const addNewProduct = (product: Omit<Product, 'rating'>): void => {
+		dispatch(addProduct(product));
+	};
+
 	const deleteProduct = (id: number): void => {
 		dispatch(deleteProductById(id));
 	};
-	// const allAllProducts = async (): Promise<void> => {
-	// 	try {
-	// 		const PRODUCTS = await getAllProducts();
 
-	// 		if (!PRODUCTS) {
-	// 			throw new Error('Failed to fetch products');
-	// 		}
-
-	// 		dispatch(addAllOfProducts(PRODUCTS));
-	// 	} catch (error) {
-	// 		if (error instanceof Error) {
-	// 			throw new TypeError(error.message);
-	// 		}
-	// 	}
-	// };
-
-	return { deleteProduct /*addAllProducts: allAllProducts*/ };
+	return { deleteProduct, addNewProduct };
 };
