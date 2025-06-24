744c0b28cacfce7fdae23a7472d8d939fde73d0d
galeed gutierrez <galeedeveloper@gmail.com>
2025-06-24 20:46:27 -0300

    feat: add initial UserProfileSchema structure for user validation

---
+++ src/sections/users/validations/UserProfileSchema.ts
@@ -0,0 +1,20 @@
+/* eslint-disable unicorn/no-empty-file */
+// import * as z from 'zod';
+
+// export const UserProfileSchema = z.object({
+// 	email: z.string().email({
+// 		message: 'El correo electrónico debe ser válido',
+// 	}),
+// 	username: z
+// 		.string()
+// 		.min(2, {
+// 			message: 'El nombre de usuario debe tener al menos 2 caracteres',
+// 		})
+// 		.optional(),
+// 	password: z.string().min(6, {
+// 		message: 'La contraseña debe tener al menos 6 caracteres',
+// 	}),
+// });
+// export type UserProfileSchemaData = z.infer<typeof UserProfileSchema>;
+
+// Si tenemos tiempo lo implementamos
