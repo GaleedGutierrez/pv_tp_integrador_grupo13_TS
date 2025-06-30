b2f0764698f3568b40ea8009dd77800f04ae62d7
galeed gutierrez <galeedeveloper@gmail.com>
2025-06-30 09:30:21 -0300

    feat(PublicApp): create PublicApp component with routing for Login and Register

---
+++ src/views/public/PublicApp.tsx
@@ -0,0 +1,28 @@
+// views/public/PublicApp.tsx
+import { appRoutes } from '@routes/appRouters';
+import type { JSX } from 'react';
+import { Navigate, Route, Routes } from 'react-router';
+
+import { Layout } from '../Layout';
+import { Login } from './Login';
+import { Register } from './Register';
+
+export const PublicApp = (): JSX.Element => (
+	<Routes>
+		<Route element={<Layout />}>
+			<Route
+				element={<Login />}
+				path={appRoutes.public.login}
+			/>
+			<Route
+				element={<Register />}
+				path={appRoutes.public.register}
+			/>
+
+			<Route
+				element={<Navigate to={appRoutes.public.login} />}
+				path="*"
+			/>
+		</Route>
+	</Routes>
+);
