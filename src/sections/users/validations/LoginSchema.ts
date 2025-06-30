f9b7caa3a38a4ad04d73f23d4a0179f03b1a46ee
galeed gutierrez <galeedeveloper@gmail.com>
2025-06-29 23:02:29 -0300

    fix(LoginSchema): enhance email and password validation with required errors and non-empty checks

--- src/sections/users/validations/LoginSchema.ts
+++ src/sections/users/validations/LoginSchema.ts
@@ -1,9 +1,17 @@
+/* eslint-disable camelcase */
 import * as z from 'zod';
 
 export const LoginSchema = z.object({
-	email: z.string().email({
-		message: 'Verifica el correo electrónico ingresado.',
-	}),
-	password: z.string(),
+	email: z
+		.string({ required_error: 'El correo electrónico es obligatorio.' })
+		.nonempty({ message: 'El correo electrónico no puede estar vacío.' })
+		.email({
+			message: 'Verifica el correo electrónico ingresado.',
+		}),
+	password: z
+		.string({ required_error: 'La contraseña es obligatoria.' })
+		.nonempty({
+			message: 'La contraseña no puede estar vacía.',
+		}),
 });
 export type LoginSchemaData = z.infer<typeof LoginSchema>;
