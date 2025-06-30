450d4e6cc1294cce855d3ee9501337c11f627835
galeed gutierrez <galeedeveloper@gmail.com>
2025-06-30 10:53:46 -0300

    feat(Navbar): enhance navigation logic to conditionally render links based on authentication status

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
+import { useAuthContext } from '@context/auth.context';
 import { appRoutes } from '@routes/appRouters';
-import type { JSX } from 'react';
+import { UserIcon } from '@ui/user';
+import { type JSX } from 'react';
 import { Link } from 'react-router';
 
 interface Properties {
@@ -11,35 +13,68 @@ interface Properties {
  * This component renders a navigation bar with links to different sections of the application.
  * @returns The rendered component with routes and a not found page.
  * */
-export const Navbar = ({ isMenuOpen }: Properties): JSX.Element => (
-	<nav
-		className={`${isMenuOpen ? 'block' : 'hidden'} fixed inset-x-0 top-18 z-1 w-full border-b border-gray-200 bg-white p-5 lg:static lg:block lg:w-fit lg:border-none lg:bg-transparent lg:p-0`}
-	>
-		<ul className="m-auto flex flex-col items-center gap-2 lg:m-0 lg:flex-row lg:items-start">
-			<li>
-				<Link
-					className="rounded-lg p-2 text-2xl hover:bg-gray-100 active:bg-transparent active:underline lg:text-base"
-					to={appRoutes.home.index}
-				>
-					Home
-				</Link>
-			</li>
-			<li>
-				<Link
-					className="rounded-lg p-2 text-2xl hover:bg-gray-100 active:bg-transparent active:underline lg:text-base"
-					to={appRoutes.favorites.list}
-				>
-					Favoritos
-				</Link>
-			</li>
-			<li>
-				<Link
-					className="rounded-lg p-2 text-2xl hover:bg-gray-100 active:bg-transparent active:underline lg:text-base"
-					to={appRoutes.products.routes.create}
-				>
-					+ Añadir Producto
-				</Link>
-			</li>
-		</ul>
-	</nav>
-);
+export const Navbar = ({ isMenuOpen }: Properties): JSX.Element => {
+	const { auth } = useAuthContext();
+	const { isLoggedIn } = auth;
+
+	return (
+		<nav
+			className={`${isMenuOpen ? 'block' : 'hidden'} fixed inset-x-0 top-18 z-1 w-full border-b border-gray-200 bg-white p-5 lg:static lg:block lg:w-fit lg:border-none lg:bg-transparent lg:p-0`}
+		>
+			<ul className="m-auto flex flex-col items-center justify-center gap-2 lg:m-0 lg:flex-row lg:items-start">
+				{isLoggedIn ? (
+					<>
+						<li>
+							<Link
+								className="rounded-lg p-2 text-2xl hover:bg-gray-100 active:bg-transparent active:underline lg:text-base"
+								to={appRoutes.private.home}
+							>
+								Home
+							</Link>
+						</li>
+						<li>
+							<Link
+								className="rounded-lg p-2 text-2xl hover:bg-gray-100 active:bg-transparent active:underline lg:text-base"
+								to={appRoutes.private.favorites.list}
+							>
+								Favoritos
+							</Link>
+						</li>
+						<li>
+							<Link
+								className="rounded-lg p-2 text-2xl hover:bg-gray-100 active:bg-transparent active:underline lg:text-base"
+								to={appRoutes.private.products.routes.create}
+							>
+								+ Añadir Producto
+							</Link>
+						</li>
+						<li>
+							<button>
+								<UserIcon size={20} />
+							</button>
+						</li>
+					</>
+				) : (
+					<>
+						<li>
+							<Link
+								className="rounded-lg p-2 text-2xl hover:bg-gray-100 active:bg-transparent active:underline lg:text-base"
+								to={appRoutes.public.login}
+							>
+								Iniciar sesión
+							</Link>
+						</li>
+						<li>
+							<Link
+								className="rounded-lg p-2 text-2xl hover:bg-gray-100 active:bg-transparent active:underline lg:text-base"
+								to={appRoutes.public.register}
+							>
+								Registrarse
+							</Link>
+						</li>
+					</>
+				)}
+			</ul>
+		</nav>
+	);
+};
