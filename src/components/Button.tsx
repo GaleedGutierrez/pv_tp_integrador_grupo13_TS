3e21bb4cd232b254f5b7e8fdda4afffc989adb6f
galeed gutierrez <galeedeveloper@gmail.com>
2025-07-04 13:06:15 -0300

    fix(Button): remove trailing spaces in className assignments for consistency

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -22,11 +22,11 @@ export const Button = ({
 		'py-4 w-full rounded-full font-semibold focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2';
 
 	if (isPrimary) {
-		properties.className = `${CLASS_NAME} ${BASE_STYLES} text-white bg-gray-950 hover:bg-gray-800 active:bg-gray-700  focus:ring-gray-500 `;
+		properties.className = `${CLASS_NAME} ${BASE_STYLES} text-white bg-gray-950 hover:bg-gray-800 active:bg-gray-700  focus:ring-gray-500`;
 	}
 
 	if (isSecondary) {
-		properties.className = `${CLASS_NAME} ${BASE_STYLES} border border-gray-300 hover:bg-gray-100 active:bg-gray-200 focus:ring-gray-500 `;
+		properties.className = `${CLASS_NAME} ${BASE_STYLES} border border-gray-300 hover:bg-gray-100 active:bg-gray-200 focus:ring-gray-500`;
 	}
 
 	return <button {...properties}>{children}</button>;
