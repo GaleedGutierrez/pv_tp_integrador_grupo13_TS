f3492c1b6a787d55582e4ce6a1d5bc8bab4a5e5e
galeed gutierrez <galeedeveloper@gmail.com>
2025-07-04 16:02:11 -0300

    fix(routes): correct spelling of the registration route in UsersRoutes

--- src/modules/users/infrastructure/UsersRoutes.ts
+++ src/modules/users/infrastructure/UsersRoutes.ts
@@ -1,5 +1,5 @@
 export const UsersRoutes = {
-	register: `${import.meta.env.BASE_URL}registrarse`,
+	register: `${import.meta.env.BASE_URL}registrar`,
 	login: `${import.meta.env.BASE_URL}iniciar-sesion`,
 } as const;
 export type TypeUsersRoutes = (typeof UsersRoutes)[keyof typeof UsersRoutes];
